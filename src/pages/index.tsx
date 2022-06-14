a0cf2500fd71966aa98f8634579c843ce85c49ed
Andres <zadorozhnyi99.dev@gmail.com>
2022-06-14 13:01:21 +0200

    trpc setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import { trpc } from '@/utils/trpc'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+   const { data, isLoading } = trpc.useQuery(['hello', { text: 'Andrei' }])
+   if (isLoading) return <p>Loading...</p>
+   if (data) return <div className="text-center">{data.greeting}</div>
    return <div className="text-center">Hello</div>
 }
 
