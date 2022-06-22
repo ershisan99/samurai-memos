f094d9a76dd70be19888bdffe67c8110ab14c22c
Andres <zadorozhnyi99.dev@gmail.com>
2022-06-22 20:47:22 +0200

    fix homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,11 @@
-import { RenderHtml } from '@/components/RenderHtml'
-import { trpc } from '@/utils/trpc'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-   const { data, isLoading } = trpc.useQuery(['getAllPosts'])
-
-   if (isLoading || !data) return <p>Loading...</p>
-
-   return (
-      <div className="p-10 prose">
-         <h1>Home</h1>
-
-         {data.map((post) => {
-            return (
-               <div key={post.id} className="p-10">
-                  <RenderHtml html={post.content} title={post.title} />
-
-                  <>
-                     <p>status: {post.status}</p>
-                     <p>created: {post.createdAt?.toLocaleString()}</p>
-                     <p>category: {post.category.name}</p>
-                  </>
-               </div>
-            )
-         })}
-      </div>
-   )
+    return (
+        <div className='p-10 prose'>
+            <h1>Home</h1>
+        </div>
+    )
 }
 
 export default Home
